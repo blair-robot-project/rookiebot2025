f7433614552dff68b9706949b3194e9a1c6c68ac
5thDimension-Sean <ss0486034@gmail.com>
2025-10-30 16:23:50 -0400

    Conveyor functions control/constants

---
+++ src/main/java/frc/robot/subsystems/Conveyor.kt
@@ -0,0 +1,32 @@
+package frc.robot.subsystems.conveyor
+
+import com.revrobotics.spark.SparkBase
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkMax
+import com.revrobotics.spark.config.SparkBaseConfig
+import com.revrobotics.spark.config.SparkMaxConfig
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.Constants
+import frc.robot.subsystems.Conveyor.ConveyorConstants
+import frc.robot.subsystems.Conveyor.ConveyorConstants.CONVEYOR_ID
+
+class Intake (
+
+)   : SubsystemBase(){
+    val conveyor = SparkMax(ConveyorConstants.CONVEYOR_ID, SparkLowLevel.MotorType.kBrushless)
+    val config = SparkMaxConfig()
+    init{
+        config.smartCurrentLimit(ConveyorConstants.CURRENT_LIMIT)
+        config.idleMode(SparkBaseConfig.IdleMode.kCoast)
+        //config.inverted(True)
+        conveyor.configure(config,SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
+    }
+    //Movement measured in voltage
+    fun move(volt:Double){
+        return conveyor.setVoltage(volt)
+    }
+    //Stops motor
+    fun stop(){
+        return conveyor.setVoltage(0.0)
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/ConveyorConstants.kt
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.Conveyor
+
+object ConveyorConstants{
+    //ALL PLACEHOLDERS
+    const val CONVEYOR_ID = 3
+    const val CURRENT_LIMIT = 40
+}
