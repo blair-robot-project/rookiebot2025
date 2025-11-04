d2c756b2c52faf2706f6e4d330789145db9ae33c
Sakai12404 <nathansanchez745@gmail.com>
2025-11-04 16:22:25 -0500

    Updated SwerveDrive with correct ID

--- src/main/java/frc/robot/subsystems/constants/SwerveDriveConstants.kt
+++ src/main/java/frc/robot/subsystems/constants/SwerveDriveConstants.kt
@@ -10,9 +10,13 @@ object SwerveDriveConstants {
     val m_backRightLocation = Translation2d(-0.351,0.351)
 
     const val frontLeftMotorID = 1
+    const val frontLeftTurnMotorID = 5
     const val frontRightMotorID = 2
+    const val frontRightTurnMotorID = 6
     const val backLeftMotorID = 3
+    const val backLeftTurnMotorID = 7
     const val backRightMotorID = 4
+    const val backRightTurnMotorID = 8
 
     const val kp = 1.0
     const val ki = 1.0
