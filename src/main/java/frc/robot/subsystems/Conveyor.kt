9d82c562236b64c77eec00d00f5a17adbc55970e
Ethan <ethanaronkouam@gmail.com>
2025-11-11 15:52:20 -0500

    added 2nd motor

--- src/main/java/frc/robot/subsystems/Conveyor.kt
+++ src/main/java/frc/robot/subsystems/Conveyor.kt
@@ -5,6 +5,7 @@ import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.SparkMax
 import com.revrobotics.spark.config.SparkBaseConfig
 import com.revrobotics.spark.config.SparkMaxConfig
+import edu.wpi.first.wpilibj.motorcontrol.Spark
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.Constants
 import frc.robot.subsystems.Conveyor.ConveyorConstants
@@ -14,9 +15,15 @@ class Conveyor (
 )   : SubsystemBase(){
     val conveyor = SparkMax(ConveyorConstants.CONVEYOR_ID, SparkLowLevel.MotorType.kBrushless)
     val config = SparkMaxConfig()
-    init{
+    val conveyor2 = SparkMax(ConveyorConstants.CONVEYOR_ID_2, SparkLowLevel.MotorType.kBrushless)
+    val config2 = SparkMaxConfig()
+    init {
         config.smartCurrentLimit(ConveyorConstants.CURRENT_LIMIT)
-        config.idleMode(SparkBaseConfig.IdleMode.kCoast)
+        config2.smartCurrentLimit(ConveyorConstants.CURRENT_LIMIT)
+        config.idleMode(SparkBaseConfig.IdleMode.kBrake)
+        config2.idleMode(SparkBaseConfig.IdleMode.kBrake)
+        config2.follow(conveyor, true)
+        conveyor2.configure(config2, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
         //config.inverted(True)
         conveyor.configure(config,SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
     }
@@ -24,6 +31,7 @@ class Conveyor (
     fun move(volt:Double){
         return conveyor.setVoltage(volt)
     }
+
     //Stops motor
     fun stop(){
         return conveyor.setVoltage(0.0)
--- src/main/java/frc/robot/subsystems/ConveyorConstants.kt
+++ src/main/java/frc/robot/subsystems/ConveyorConstants.kt
@@ -3,5 +3,6 @@ package frc.robot.subsystems.Conveyor
 object ConveyorConstants{
     //ALL PLACEHOLDERS
     const val CONVEYOR_ID = 3
+    const val CONVEYOR_ID_2 = 5
     const val CURRENT_LIMIT = 40
 }
