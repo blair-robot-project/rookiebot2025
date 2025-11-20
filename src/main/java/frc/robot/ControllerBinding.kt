c94a708d2ef4b2ee4d62ff34cbbdf36bcdcbdd5a
Sakai12404 <nathansanchez745@gmail.com>
2025-11-19 21:05:02 -0500

    Finishing up jus need bindings

--- src/main/java/frc/robot/ControllerBinding.kt
+++ src/main/java/frc/robot/ControllerBinding.kt
@@ -1,12 +1,16 @@
 package frc.robot
 
 import edu.wpi.first.wpilibj.XboxController
+import frc.robot.commands.SwerveDriveCommand
 
 class ControllerBinding(
     val driveController : XboxController,
     val robot : Robot
     ) {
     private fun robotBinding(){
+
+    }
+    private fun swerveDrive(){
 
     }
 }
--- src/main/java/frc/robot/RobotContainer.kt
+++ src/main/java/frc/robot/RobotContainer.kt
@@ -46,7 +46,6 @@ object RobotContainer
     {
         // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
         Trigger { ExampleSubsystem.exampleCondition() }.onTrue(ExampleCommand())
-        Trigger { drive.}.onTrue()
         // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
         // cancelling on release.
         driverController.b().whileTrue(ExampleSubsystem.exampleMethodCommand())
