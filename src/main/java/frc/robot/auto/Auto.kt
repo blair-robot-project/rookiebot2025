701f804a3cba11637419116615c07f8655f1be90
lucas <lucas.zeng.2021@gmail.com>
2025-12-02 16:03:31 -0500

    Auto branch and files created.

---
+++ src/main/java/frc/robot/auto/Auto.kt
@@ -0,0 +1,10 @@
+package frc.robot.auto
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
+
+open class Routines {
+}
